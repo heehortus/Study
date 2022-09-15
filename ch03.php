a3654c8aa5a07df1829ef5cd48d3b92332f52433
HeeWon <108612816+heehortus@users.noreply.github.com>
2022-09-15 14:05:02 +0900

    Update ch03.php

--- ch03.php
+++ ch03.php
@@ -82,4 +82,123 @@
 	echo "17시 이후 입장 : $night <br>";
 	echo "나이 : $age <br> <br>";
 	echo "입장료 : $fee <br> <hr>";
+
+	echo "<b>예제 3-9</b> <br>";
+	//작은 것부터 정렬하기
+	$a = 10;
+	$b = 3;
+	$c = 9;
+
+	if ($a > $b)
+	{
+		if ($a > $c)
+		{
+			if($b > $c)
+			{
+				$min = $c;
+				$mid = $b;
+				$max = $a;
+			}
+
+			else //$b < $c
+			{
+				$min = $b;
+				$mid = $c;
+				$max = $a;
+			}
+		}
+
+		else //$a < $c
+		{
+			if($b > $c)
+			{
+				$min = $a;
+				$mid = $c;
+				$max = $b;
+			}
+
+			else //$b < $c
+			{
+				$min = $b;
+				$mid = $a;
+				$max = $c;
+			}
+		}
+	}
+
+	else //$a < $b
+	{
+		if ($a > $c) //$c < $a < $b 
+		{
+			if($b > $c)
+			{
+				$min = $c;
+				$mid = $a;
+				$max = $b;
+			}
+
+			else //$a < $b < $c
+			{
+				$min = $a;
+				$mid = $b;
+				$max = $c;
+			}
+		}
+
+		else //$a < $c
+		{
+			if($b > $c)	//$a < $c < $b
+			{
+				$min = $a;
+				$mid = $c;
+				$max = $b;
+			}
+
+			else //$a < $b < $c
+			{
+				$min = $a;
+				$mid = $b;
+				$max = $c;
+			}
+		}
+	}
+
+	echo "입력된 세 정수 : $a $b $c <br>";
+	echo "입력된 정수를 작은 순서대로 배열 : $min $mid $max <br> <hr>";
+
+	echo "<b>예제 3-10 </b> <br>";
+
+	$now_year = 2022;
+	$now_month = 9;
+	$now_day = 15;
+
+	$birth_year = 2000;
+	$birth_month = 7;
+	$birth_day = 27;
+
+	if ($birth_month < $now_month)	//생일이 지나지 않음
+	{
+		$age = $now_year - $birth_year;
+	}
+
+	else if ($birth_month == $now_month)	//달이 똑같음
+	{
+		if($birth_day <= $now_day)
+		{
+			$age = $now_year - $birth_year;
+		}
+
+		else //생일이 지남
+			$age = $now_year - $birth_year - 1;
+	}
+
+	else
+	{	
+		$age = $now_year - $birth_year - 1;
+	}
+	echo "오늘 날짜 : $now_year 년 $now_month 월 $now_day 일 <br>";
+	echo "출생 연월일 : $birth_year 년 $birth_month 월 $birth_day 일 <br>";
+	echo "만 나이 : $age <br> <hr>";
+
+	
 ?>
